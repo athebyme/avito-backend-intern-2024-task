c89878dbc505fbef1dbba864c5c627ed90a0beba
athebyme <bruhov37@mail.ru>
2024-09-11 12:59:44 +0300

    added:
    @Id
    - annotation

--- src/main/java/io/codefresh/gradleexample/dao/entities/organizations/OrganizationEntity.java
+++ src/main/java/io/codefresh/gradleexample/dao/entities/organizations/OrganizationEntity.java
@@ -1,9 +1,13 @@
-package io.codefresh.gradleexample.dao.entities;
+package io.codefresh.gradleexample.dao.entities.organizations;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.sql.Timestamp;
 import java.util.UUID;
 
+@Entity
 public class OrganizationEntity {
+    @Id
     UUID id;
     String name;
     String description;
