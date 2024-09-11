a151409dcdd988a95c4939c386a71b3ff0f8feb3
athebyme <bruhov37@mail.ru>
2024-09-11 12:59:33 +0300

    added:
    @Id
    @Getter
    @Setter
    - annotations

--- src/main/java/io/codefresh/gradleexample/dao/entities/users/EmployeeEntity.java
+++ src/main/java/io/codefresh/gradleexample/dao/entities/users/EmployeeEntity.java
@@ -1,11 +1,20 @@
-package io.codefresh.gradleexample.dao.entities;
+package io.codefresh.gradleexample.dao.entities.users;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.sql.Timestamp;
 import java.util.UUID;
 
+@Entity
+@Getter
+@Setter
 public class EmployeeEntity
 {
+    @Id
     UUID id;
     String username;
     String first_name;
