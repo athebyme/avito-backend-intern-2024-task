112061d63e289813de75da4961d602b9afc27ab3
athebyme <bruhov37@mail.ru>
2024-09-12 19:09:36 +0300

    тест на переменную среды

--- src/test/java/io/codefresh/gradleexample/ContextLoadTest.java
+++ src/test/java/io/codefresh/gradleexample/ContextLoadTest.java
@@ -6,14 +6,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+
 public class ContextLoadTest {
 
     @Test
     public void contextLoads() {
         assertEquals("Expected correct message","Hello World","Hello "+"World");
+    }
+
+    @Test
+    public void testEnvVariable() {
+        String jdbcUrl = System.getenv("POSTGRES_JDBC_URL");
+        System.out.println("POSTGRES_JDBC_URL: " + jdbcUrl);
+        assertNotNull("Переменная POSTGRES_JDBC_URL не найдена", jdbcUrl);
     }
 }
 
