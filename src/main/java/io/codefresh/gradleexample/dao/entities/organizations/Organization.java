f2a5e949f3f953ec995bce5d250c9311d193bc32
athebyme <bruhov37@mail.ru>
2024-09-12 19:13:18 +0300

    добавлены аннотации для энама и явное указание генерируемого ID в БД

--- src/main/java/io/codefresh/gradleexample/dao/entities/organizations/Organization.java
+++ src/main/java/io/codefresh/gradleexample/dao/entities/organizations/Organization.java
@@ -1,16 +1,18 @@
 package io.codefresh.gradleexample.dao.entities.organizations;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.UUID;
 
 @Entity
 public class Organization {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
     UUID id;
     String name;
     String description;
+    @Enumerated(EnumType.STRING)
     OrganizationTypes type;
     Timestamp created_at;
     Timestamp updated_at;
