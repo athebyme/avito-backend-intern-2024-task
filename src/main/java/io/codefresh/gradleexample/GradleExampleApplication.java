e5551550bd5f042417d9147470d6c1518d3aa568
athebyme <bruhov37@mail.ru>
2024-09-12 11:26:50 +0300

    updated :

--- src/main/java/io/codefresh/gradleexample/GradleExampleApplication.java
+++ src/main/java/io/codefresh/gradleexample/GradleExampleApplication.java
@@ -2,9 +2,14 @@ package io.codefresh.gradleexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.Map;
 
 @SpringBootApplication
-public class GradleExampleApplication {
+public class GradleExampleApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradleExampleApplication.class, args);
