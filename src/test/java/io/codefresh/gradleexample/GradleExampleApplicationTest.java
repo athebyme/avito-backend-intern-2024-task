9b11aad2fa5e1e83af0734c18f100f38c577148f
athebyme <bruhov37@mail.ru>
2024-09-12 12:47:26 +0300

    testing naming errors

--- src/test/java/io/codefresh/gradleexample/GradleExampleApplicationTest.java
+++ src/test/java/io/codefresh/gradleexample/GradleExampleApplicationTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ContextLoadTest {
+public class GradleExampleApplicationTest {
 
     @Test
     public void contextLoads() {
