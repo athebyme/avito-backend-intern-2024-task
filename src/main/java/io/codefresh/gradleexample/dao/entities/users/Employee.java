2f3541575f9ed240b96f9a64f65f58d65fdaa539
athebyme <bruhov37@mail.ru>
2024-09-12 19:09:50 +0300

    явное указание таблицы

--- src/main/java/io/codefresh/gradleexample/dao/entities/users/Employee.java
+++ src/main/java/io/codefresh/gradleexample/dao/entities/users/Employee.java
@@ -5,13 +5,16 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.sql.Timestamp;
 import java.util.UUID;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "employee")
 public class Employee
 {
     @Id
