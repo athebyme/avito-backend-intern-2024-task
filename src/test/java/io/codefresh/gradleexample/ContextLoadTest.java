ddc27caefe7e28ab41158b4538e66cf53838c6c8
athebyme <bruhov37@mail.ru>
2024-09-13 15:38:16 +0300

    билд проверка

--- src/test/java/io/codefresh/gradleexample/ContextLoadTest.java
+++ src/test/java/io/codefresh/gradleexample/ContextLoadTest.java
@@ -1,14 +1,11 @@
 package io.codefresh.gradleexample;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 
 public class ContextLoadTest {
