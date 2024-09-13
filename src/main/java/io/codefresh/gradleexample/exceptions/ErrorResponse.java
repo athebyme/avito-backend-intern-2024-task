7e9c40e6910860ae44e9127656930461b8e043e8
athebyme <bruhov37@mail.ru>
2024-09-13 13:40:28 +0300

    сущность ошибки (пока не знаю нужно ли)

--- src/main/java/io/codefresh/gradleexample/exceptions/ErrorResponse.java
+++ src/main/java/io/codefresh/gradleexample/exceptions/ErrorResponse.java
@@ -1,11 +1,13 @@
 package io.codefresh.gradleexample.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class ErrorResponse {
+    @JsonProperty("reason")
     private String reason;
 
     public ErrorResponse(String reason) {
