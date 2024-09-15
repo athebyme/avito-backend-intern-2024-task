af8852306cbaa93b47a19818b6f9694822d48a36
athebyme <bruhov37@mail.ru>
2024-09-15 21:44:01 +0300

    new updates

--- src/main/java/io/codefresh/gradleexample/web/controllers/BidsController.java
+++ src/main/java/io/codefresh/gradleexample/web/controllers/BidsController.java
@@ -22,7 +22,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
